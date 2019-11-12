4a3a89bb81049205577a73853957f8fb82c65c2c
lynx <2104934038@qq.com>
2019-11-12 18:03:31 +0800

    todo:密码错误,账号不存在

--- src/main/java/com/example/springweb/controller/HelloController.java
+++ src/main/java/com/example/springweb/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.springweb.controller;
 
+import com.example.springweb.dao.HelloUser;
 import com.example.springweb.service.HelloService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,21 +19,32 @@ import java.util.Set;
 public class HelloController {// 控制页面跳转,连接数据库
     @Autowired
     HelloService helloService;
+    List<HelloUser> allUsers;// 记录所有用户
     public final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     // 从主页跳转到任何页面
     @RequestMapping("/{page}")
     public String changePage(@PathVariable("page") String page) {
-        infoLog("hello logging" + helloService.getUserList());
         infoLog("from index");
         return page;
     }
 
     @RequestMapping("/enterprise/{page}")
     public String login(GetLogin getLogin, @PathVariable("page") String page) {
-        infoLog("page: " + page);
+        allUsers = helloService.getUserList();// 更新用户列表
         infoLog("account: " + getLogin.account);
-        return "enterprise/" + page;
+        infoLog("account: " + getLogin.password);
+
+        for (int i = 0; i < allUsers.size(); i ++) {
+            if (allUsers.get(i).getId() == getLogin.account) {// 存在用户
+                if (allUsers.get(i).getPassword() == getLogin.password) {// 密码正确
+                    return "enterprise/" + page;
+                } else {
+                    return "enterprise?error=1";// TODO 密码错误
+                }
+            }
+        }
+        return "enterprise?error=2";// TODO 账号不存在
     }
 
     public void infoLog(String log) {
