6f6b51ec29d59bddd3ccc1c9125eccc1b5916c1f
lynx <2104934038@qq.com>
2019-11-12 19:48:08 +0800

    bug:登录失败

--- src/main/java/com/example/springweb/controller/HelloController.java
+++ src/main/java/com/example/springweb/controller/HelloController.java
@@ -7,8 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -30,28 +33,33 @@ public class HelloController {// 控制页面跳转,连接数据库
     }
 
     @RequestMapping("/enterprise/{page}")
-    public String login(GetLogin getLogin, @PathVariable("page") String page, Map<String, Object> map) {
+    public String login(GetLogin getLogin, @PathVariable("page") String page, Model model) {
         infoLog("request2: " + page);// TODO 对于不同request,返回不同文件
         allUsers = helloService.getUserList();// 更新用户列表
-        infoLog(allUsers + "");
-        infoLog("account: " + getLogin.account);
-        infoLog("password: " + getLogin.password);
 
-        for (int i = 0; i < allUsers.size(); i ++) {
-            infoLog(allUsers.get(i).getId() + ": " + allUsers.get(i).getPassword());
-            if (allUsers.get(i).getId().equals(getLogin.account)) {// 存在用户
-                if (allUsers.get(i).getPassword().equals(getLogin.password)) {// 密码正确
-                    return "enterprise/" + page;// 登陆成功
-                } else {
-                    infoLog("密码错误");
-                    map.put("result", "1");
-                    return "redirect:/enterprise";// TODO 密码错误
+        if (page.equals("home.html") || page.equals("home")) {
+            infoLog(allUsers + "");
+            infoLog("account: " + getLogin.account);
+            infoLog("password: " + getLogin.password);
+
+            for (int i = 0; i < allUsers.size(); i ++) {
+                infoLog(allUsers.get(i).getId() + ": " + allUsers.get(i).getPassword());
+                if (allUsers.get(i).getId().equals(getLogin.account)) {// 存在用户
+                    if (allUsers.get(i).getPassword().equals(getLogin.password)) {// 密码正确
+                        infoLog("成功登陆");
+                        return "redirect:/enterprise/home?user=" + allUsers.get(i).getName();// 登陆成功
+                    } else {
+                        infoLog("密码错误");
+                        return "redirect:/enterprise?error=1";// TODO 密码错误
+                    }
                 }
             }
+            infoLog("账号不存在");
+            return "redirect:/enterprise?error=2";// TODO 账号不存在
+        } else {
+            return "redirect:/enterprise/" + page;
         }
-        infoLog("账号不存在");
-        map.put("result", "2");
-        return "redirect:/enterprise";// TODO 账号不存在
+
     }
 
     public void infoLog(String log) {
