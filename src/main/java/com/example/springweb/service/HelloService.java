7dbd3d233fe1dbc02b5ed5817e7828091962a65d
lynx <2104934038@qq.com>
2019-11-14 09:11:25 +0800

    bug:获取不到数据库

--- src/main/java/com/example/springweb/service/HelloService.java
+++ src/main/java/com/example/springweb/service/HelloService.java
@@ -20,49 +20,49 @@ public class HelloService {
         return list;
     }
 
-    /*public void InsertUser(HelloUser helloUser){
+    /*public void InsertUser(HelloUser helloUser) {
         helloMapper.insert(helloUser);
-        System.out.println("Afterinsert:"+helloMapper.findAll());
+        System.out.println("After insert:"+helloMapper.findAll());
     }*/
-    public void InsertUser(Map<String, String> params){
+    public void InsertUser(Map<String, String> params) {
         ObjectMapper objectMapper = new ObjectMapper();
         HelloUser helloUser = objectMapper.convertValue(params, HelloUser.class);
         helloMapper.insert(helloUser);
     }
 
 
-    public HelloUser getOne(String id){
+    public HelloUser getOne(String id) {
         //HelloUser result = new HelloUser();
         HelloUser result = helloMapper.getOne(id);
-        System.out.println("getOne:"+result);
-        if (result==null)
+        System.out.println("getOne:" + result);
+        if (result == null)
         {
-            result=new HelloUser();//索引为空的时候，返回null，需要这时候对其getId,getName就会出错。
+            result = new HelloUser();//索引为空的时候，返回null，需要这时候对其getId,getName就会出错。
         }
         System.out.println(result.toString());
         return result;
     }
 
-    /*public void UpdateByID(HelloUser helloUser){
+    /*public void UpdateByID(HelloUser helloUser) {
         helloMapper.updateByID(helloUser);
-        System.out.println("AfterUpdate:"+ helloMapper.getOne(helloUser.getId()));
+        System.out.println("After update:"+ helloMapper.getOne(helloUser.getId()));
     }*/
-    public void UpdateByID(Map<String, String> params){
+    public void UpdateByID(Map<String, String> params) {
         String id = params.get("id");
         //Long recordId = Long.parseLong(params.get("recordId"));
         //ObjectMapper objectMapper = new ObjectMapper();
         //HelloUser helloUser = objectMapper.convertValue(params, HelloUser.class);
         //helloMapper.updateByID(helloUser);
         HelloUser temp = helloMapper.getOne(id);
-        if(params.get("name")!=null)
+        if(params.get("name") != null)
             temp.setName(params.get("name"));
-        if(params.get("password")!=null)
+        if(params.get("password") != null)
             temp.setPassword((params.get("password")));
         helloMapper.updateByID(temp);
     }
 
 
-    public void DeleteByID(String id){
+    public void DeleteByID(String id) {
         helloMapper.deleteByID(id);
         System.out.println("AfterDelete:"+helloMapper.getOne(id));
     }
